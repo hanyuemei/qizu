bf23454ba34fabe82e03829f5f0486528eb34dec
unknown <599077687@qq.com>
2016-03-22 08:06:03 +0800

    ceshi

---
+++ question/models/interview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Interview extends ActiveRecord{
+
+	    public static function tableName(){
+
+	        return "direction";
+	    }
+	}
