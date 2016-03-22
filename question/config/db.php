446396f287d3cc25b4b78ebaeaf2730900afa712
unknown <599077687@qq.com>
2016-03-22 08:10:51 +0800

    del

--- question/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.1.166;dbname=questions',
-    'username' => 'user1',
-    'password' => '1111111',
-    'charset' => 'utf8',
-];
