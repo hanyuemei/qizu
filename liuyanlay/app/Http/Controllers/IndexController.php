6b7f3b2ec224eae1ec386a74b56f6fd3c83fce58
hym <2492530183@qq.com>
2016-03-24 14:16:13 +0800

    update

--- liuyanlay/app/Http/Controllers/IndexController.php
+++ liuyanlay/app/Http/Controllers/IndexController.php
@@ -11,23 +11,24 @@ class IndexController extends Controller {
         //$arr=DB::select($sql);
         //return view('index',['arr'=>$arr]);
         $arr= DB::table('liuyan')->paginate(3);
-        //print_r($arr);die;
         return view('index',['arr'=>$arr]);
     }
     public function add(){
         $con=$_GET['con'];
         $sql="insert into liuyan(c_con) values('$con')";
         DB::insert($sql);
-        $sql2="select * from liuyan";
-       $arr= DB::select($sql2);
+        //$sql2="select * from liuyan";
+       //$arr= DB::select($sql2);
+        $arr= DB::table('liuyan')->paginate(3);
         return view('index2',['arr'=>$arr]);
     }
     public function del(){
         $id=$_GET['id'];
         $sql="delete from liuyan where c_id=".$id;
         DB::delete($sql);
-        $sql2="select * from liuyan";
-        $arr= DB::select($sql2);
+       // $sql2="select * from liuyan";
+       // $arr= DB::select($sql2);
+        $arr= DB::table('liuyan')->paginate(3);
         return view('index',['arr'=>$arr]);
     }
     public function save(){
@@ -45,10 +46,12 @@ class IndexController extends Controller {
        $con=$_GET['con'];
        $sql="update liuyan set c_con='$con' where c_id=".$id;
        //echo $sql;die;
-        $sql="select * from liuyan";
-        $arr=DB::select($sql);
+        DB::update($sql);
+       // $sql2="select * from liuyan";
+       // $arr=DB::select($sql2);
         //print_r($arr);die;
         //return view('index')->with('arr',$arr);
+        $arr= DB::table('liuyan')->paginate(3);
         return view('index',['arr'=>$arr]);
     }
 }
